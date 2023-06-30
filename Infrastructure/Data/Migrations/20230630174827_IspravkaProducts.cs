da858a7bebdcea7e1db7c20d04b6cc9227c6f2cd
samamostar97 <samir.obradovic@edu.fit.ba>
2023-06-30 20:16:55 +0200

    Added entities, first migration

---
+++ Infrastructure/Data/Migrations/20230630174827_IspravkaProducts.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IspravkaProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Porducts",
+                table: "Porducts");
+
+            migrationBuilder.RenameTable(
+                name: "Porducts",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Porducts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Porducts",
+                table: "Porducts",
+                column: "Id");
+        }
+    }
+}
